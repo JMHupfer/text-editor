3ebdb169c00c495c71d386e725e88d642f3f7c7d
JMHupfer <jmhupfer@gmail.com>
2023-07-30 17:48:40 -0600

    updated code

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,45 +1,46 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+const { InjectManifest } = require("workbox-webpack-plugin");
 
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: "development",
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
+      main: "./src/js/index.js",
+      install: "./src/js/install.js",
     },
     output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
     },
     plugins: [
       new HtmlWebpackPlugin({
-        template: './index.html',
-        chunks: ['main'], // Generate the HTML file only for the 'main' entry point
+        template: "./index.html",
+        chunks: ["main"], // Generate the HTML file only for the 'main' entry point
       }),
       new WebpackPwaManifest({
         fingerprints: false,
-        inject: false,
-        name: 'Text Editor',
-        short_name: 'App',
-        description: 'Editor for text editing',
-        background_color: '#ffffff',
-        theme_color: '#000000',
-        start_url: '/',
+        inject: true,
+        name: "Just Another Text Editor",
+        short_name: "JATE",
+        description: "Just another text editor",
+        background_color: "#225ca3",
+        theme_color: "#225ca3",
+        start_url: "/",
+        publicPath: "/",
         icons: [
           {
-            src: path.resolve('src/images/logo.png'),
+            src: path.resolve("src/images/logo.png"),
             sizes: [96, 128, 192, 256, 384, 512],
-            destination: path.join('assets', 'icons'),
+            destination: path.join("assets", "icons"),
           },
         ],
       }),
       new InjectManifest({
-        swSrc: './src-sw.js',
-        swDest: 'sw.js',
-        chunks: ['main'], // Generate the service worker only for the 'main' entry point
+        swSrc: "./src-sw.js",
+        swDest: "sw.js",
+        chunks: ["main"], // Generate the service worker only for the 'main' entry point
       }),
     ],
 
@@ -47,15 +48,19 @@ module.exports = () => {
       rules: [
         {
           test: /\.css$/,
-          use: ['style-loader', 'css-loader'],
+          use: ["style-loader", "css-loader"],
         },
         {
           test: /\.js$/,
           exclude: /node_modules/,
           use: {
-            loader: 'babel-loader',
+            loader: "babel-loader",
             options: {
-              presets: ['@babel/preset-env'],
+              presets: ["@babel/preset-env"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
+              ],
             },
           },
         },
@@ -63,4 +68,3 @@ module.exports = () => {
     },
   };
 };
-
